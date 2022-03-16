a292ac7908228d467e65c3bb82bc6a760914fd04
Himanshu Mishra <himanshumishra15@gmail.com>
2022-03-16 12:43:07 +0530

    merge_2

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IEntityBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using Ordering.Domain.Common;
+using System;
+using System.Linq.Expressions;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    internal interface IEntityBaseRepository<T> where T : EntityBase
+    {
+        Task<IReadOnlyList<T>> GetAllEntityBaseAsync();
+        Task<IReadOnlyList<T>> GetEntityBaseAsync(Expression<Func<T, bool>> predicate);
+        Task<IReadOnlyList<T>> GetEntityBaseAsync(Expression<Func<T, bool>> predicate = null,
+                                                    Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                                    string includeString = null,
+                                                    bool disableTracking = true);
+        Task<IReadOnlyList<T>> GetEntityBaseAsync(Expression<Func<T, bool>> predicate = null,
+                                                   Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                                   List<Expression<Func<T, object>>> includes = null,
+                                                   bool disableTracking = true);
+        Task<T> GetEntityBaseByIdAsync(int id);
+        Task<T> AddEntityBaseAsync(T entity);
+        Task UpdateEntityBaseAsync(T entity);
+        Task DeleteEntityBaseAsync(T entity);
+    }
+}
--- src/Services/Ordering/Ordering.Domain/Entities/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -1,14 +1,24 @@
 ﻿using Ordering.Domain.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ordering.Domain.Entities
 {
     public class Order : EntityBase
     {
+        public string UserName { get; set; }
+        public string TotalPrice { get; set; }
 
+        //Billing Address
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string PinCode { get; set; }
+
+        //Payment
+        public string BankName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
     }
 }
